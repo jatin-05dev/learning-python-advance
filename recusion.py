8db9fb8dd2ac7f46e4602f1bb569829253a4023d
jatin-05dev <jatindev214@gmail.com>
2025-11-27 16:02:58 +0530

    recursion

---
+++ recusion.py
@@ -0,0 +1,111 @@
+# natural number throygh recursion 
+# recusion reverse
+# statck
+# def natural(n):
+#     if(n==0):
+#         return
+#     natural(n-1)
+#     print(n)
+
+
+# n=int(input("enter a number : "))
+# natural(n)
+
+# stack me kam krata he 
+
+# def natural(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     natural(n-1)
+
+
+# n=int(input("enter a number : "))
+# natural(n)
+
+# neven numbr
+# 10 even oddd 
+# def natural(n):
+#     if(n==0):
+#         return
+#     print(n*2-1)
+#     natural(n-1)
+
+
+# n=int(input("enter a number : "))
+# natural(n)
+# 10 tk ke
+
+# def natural(n):
+#     if(n==0):
+#         return
+#     natural(n-1)
+#     if(n%2==0):
+#      print(n)
+
+
+# n=int(input("enter a number : "))
+# natural(n)
+
+# n natural number sum
+# 0 se none 
+# print hit hoga
+# print se na hoga
+# def natural(n):
+#     if(n==1):
+#         return 1
+#     # print(n+natural(n-1))
+#     return n+natural(n-1)
+
+# n=int(input("enter a number : "))
+# print(natural(5))
+
+# even nnumbe sum
+# def natural(n):
+#     if(n==1):
+#         return 1
+#     # print(n+natural(n-1))
+#     return n*2-1+natural(n-1)
+
+# n=int(input("enter a number : "))
+# # res=natural(n)
+# print(natural(n))
+
+# factorial
+
+# def natural(n):
+#     if(n==1):
+#         return 1
+#     # print(n+natural(n-1))
+#     return n*natural(n-1)
+
+# n=int(input("enter a number : "))
+# # res=natural(n)
+# print(natural(n))
+
+# end point likhna hoadd
+# odd even
+# call mechange to return me bhi change
+# def natural(n):
+#     if(n==1):
+#         return 2*n
+#     # print(n+natural(n-1))
+#     return 2*n+natural(n-1)
+
+# n=int(input("enter a number : "))
+# # res=natural(n)
+# print(natural(n))
+
+
+# nautral factorial
+
+# def natural(n):
+    # if(n==1):
+        # return 1
+    # print(n+natural(n-1))
+    # return n*natural(n-1)
+
+# n=int(input("enter a number : "))
+# res=natural(n)
+# print(natural(n))
+
