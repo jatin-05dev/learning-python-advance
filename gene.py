f43484b0ea00ed146196fe817d36f9144e76a229
jatin-05dev <jatindev214@gmail.com>
2026-08-03 20:38:13 +0530

    code

--- gene.py
+++ gene.py
@@ -24,3 +24,32 @@ def generate_valid_code():
 if __name__ == "__main__":
     for _ in range(5):
         print(generate_valid_code())
+
+
+
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    user_length = int(input("Enter password length: "))
+    print("Generated Password:", generate_password(user_length))
+
+
+
+prefix = "AB"
+number = 1
+
+def generate_valid_code():
+    global number
+    code = f"{prefix}{number:03d}"
+    number += 1
+    return code
+
+if __name__ == "__main__":
+    for _ in range(5):
+        print(generate_valid_code())
